0485f7f1bcfea3db00046ceebdebd9757fa5ac16
dzikrul-haq <66218103+dzikrul-haq@users.noreply.github.com>
2020-06-01 23:49:41 +0700

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasTeoriPBO;
+
+/**
+ *
+ * @author Ezik Haq Wahid
+ */
+public class Final {
+     public static void main(String[] args) {
+        final int jumlah = 100;
+        jumlah = 100 ;
+        //Error
+        //Karena nilai dari suatu variabel final tidak dapat diubah setelah sebelumnya ditetapkan
+    }
+}
